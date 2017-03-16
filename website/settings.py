8093839ef2b105fc130339fa4ee8857f7cda7597
Kamil <gallkam@outlook.com>
2017-03-16 04:39:46 +0300

    Update #16 Debugging activated

--- website/settings.py
+++ website/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rha7*we-io_dbhnf$k)%wum_i=-fct9+n^j+@5j9p*$z01lzj1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ujinjinjin.pythonanywhere.com',
                  '127.0.0.1',
